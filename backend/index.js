c9dea8d5f597232ecf7a7d5169eee2770949a175
Ranjan Mishra <mishrag7msd@gmail.com>
2024-08-06 14:21:45 +0530

    Database Setup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-const PORT = 3000;
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+dotenv.config({});
+
 const app = express();
 
 // middleware
@@ -19,6 +22,8 @@ app.get('/home',(req,res)=>{
     res.send('Hey There from Backend')
 })
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`http://localhost:${PORT}`);
 })
