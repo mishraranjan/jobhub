2b3b2d2837ce53a2932cbfb1588bc02557dfc8d6
Ranjan Mishra <mishrag7msd@gmail.com>
2024-09-29 18:54:37 +0530

    Changed Backend Url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,7 @@
-export const USER_API_END_POINT = 'http://localhost:8000/api/vi/user'
-export const JOB_API_END_POINT ='http://localhost:8000/api/vi/job'
-export const APPLICATION_API_END_POINT = 'http://localhost:8000/api/vi/application'
-export const COMPANY_API_END_POINT = 'http://localhost:8000/api/vi/company'
+import { BACKEND_LINK } from "./url"
+
+export const USER_API_END_POINT = `${BACKEND_LINK}/api/vi/user`
+export const JOB_API_END_POINT =`${BACKEND_LINK}/api/vi/job`
+export const APPLICATION_API_END_POINT = `${BACKEND_LINK}/api/vi/application`
+export const COMPANY_API_END_POINT = `${BACKEND_LINK}/api/vi/company`
 
---
+++ frontend/src/utils/url.js
@@ -0,0 +1,1 @@
+export const BACKEND_LINK ='https://jobhub-backend-0ppo.onrender.com'
